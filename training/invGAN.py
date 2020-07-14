cfba8e32d0b4f4dcf2320de910fd73d3aba5dd80
frl <frl1996@mail.ustc.edu.cn>
2020-07-14 23:12:33 +0800

    's'

--- training/invGAN.py
+++ training/invGAN.py
@@ -604,6 +604,7 @@ def Q_infer(
 
     x = images_in
     for res in range(resolution_log2, 3 - 1, -1):
+        print(res)
         with tf.variable_scope('%dx%d' % (2**res, 2**res)):
             if res == resolution_log2:
                 x = inv_torgb(res, x)
@@ -618,131 +619,6 @@ def Q_infer(
     assert latents.dtype == tf.as_dtype(dtype)
     return tf.identity(latents, name='latents_infered')
 
-
-
-
-
-#----------------------------------------------------------------------------
-# StyleGAN2 synthesis network (Figure 7).
-# Implements skip connections and residual nets (Figure 7), but no progressive growing.
-# Used in configs E-F (Table 1).
-
-def G_synthesis_stylegan2(
-    dlatents_in,                        # Input: Disentangled latents (W) [minibatch, num_layers, dlatent_size].
-    dlatent_size        = 512,          # Disentangled latent (W) dimensionality.
-    num_channels        = 3,            # Number of output color channels.
-    resolution          = 1024,         # Output resolution.
-    fmap_base           = 16 << 10,     # Overall multiplier for the number of feature maps.
-    fmap_decay          = 1.0,          # log2 feature map reduction when doubling the resolution.
-    fmap_min            = 1,            # Minimum number of feature maps in any layer.
-    fmap_max            = 512,          # Maximum number of feature maps in any layer.
-    randomize_noise     = True,         # True = randomize noise inputs every time (non-deterministic), False = read noise inputs from variables.
-    architecture        = 'skip',       # Architecture: 'orig', 'skip', 'resnet'.
-    nonlinearity        = 'lrelu',      # Activation function: 'relu', 'lrelu', etc.
-    dtype               = 'float32',    # Data type to use for activations and outputs.
-    resample_kernel     = [1,3,3,1],    # Low-pass filter to apply when resampling activations. None = no filtering.
-    fused_modconv       = True,         # Implement modulated_conv2d_layer() as a single fused op?
-    **_kwargs):                         # Ignore unrecognized keyword args.
-
-    resolution_log2 = int(np.log2(resolution))
-    assert resolution == 2**resolution_log2 and resolution >= 4
-    def nf(stage): return np.clip(int(fmap_base / (2.0 ** (stage * fmap_decay))), fmap_min, fmap_max)
-    assert architecture in ['orig', 'skip', 'resnet']
-    act = nonlinearity
-    num_layers = resolution_log2 * 2 - 2
-    images_out = None
-
-    # Primary inputs.
-    dlatents_in.set_shape([None, num_layers, dlatent_size])
-    dlatents_in = tf.cast(dlatents_in, dtype)
-
-    # Noise inputs.
-    noise_inputs = []
-    for layer_idx in range(num_layers - 1):
-        res = (layer_idx + 5) // 2
-        shape = [1, 1, 2**res, 2**res]
-        noise_inputs.append(tf.get_variable('noise%d' % layer_idx, shape=shape, initializer=tf.initializers.random_normal(), trainable=False))
-
-    # Single convolution layer with all the bells and whistles.
-    # def layer(x, layer_idx, fmaps, kernel, up=False):
-    #     x = modulated_conv2d_layer(x, dlatents_in[:, layer_idx], fmaps=fmaps,
-    #                                kernel=kernel, up=up, resample_kernel=resample_kernel, fused_modconv=fused_modconv)
-    #     if randomize_noise:
-    #         noise = tf.random_normal([tf.shape(x)[0], 1, x.shape[2], x.shape[3]], dtype=x.dtype)
-    #     else:
-    #         noise = tf.cast(noise_inputs[layer_idx], x.dtype)
-    #     noise_strength = tf.get_variable('noise_strength', shape=[], initializer=tf.initializers.zeros())
-    #     x += noise * tf.cast(noise_strength, x.dtype)
-    #     return apply_bias_act(x, act=act)
-    def layer(x, layer_idx, fmaps, up=False):
-        s = dlatents_in[:, layer_idx]
-        s = inv_dense_layer(s, fmaps=fmaps, weight_var='mod_weight')
-        s = inv_bias_act(s, bias_var='mod_bias') + 1
-        x *= tf.cast(s[:, np.newaxis, np.newaxis, :], x.dtype)
-        x = inv_module_conv2d_layer(x, up)
-        return x
-
-    # Building blocks for main layers.
-    # def block(x, res): # res = 3..resolution_log2
-    #     t = x
-    #     with tf.variable_scope('Conv0_up'):
-    #         x = layer(x, layer_idx=res*2-5, fmaps=nf(res-1), kernel=3, up=True)
-    #     with tf.variable_scope('Conv1'):
-    #         x = layer(x, layer_idx=res*2-4, fmaps=nf(res-1), kernel=3)
-    #     if architecture == 'resnet':
-    #         with tf.variable_scope('Skip'):
-    #             t = conv2d_layer(t, fmaps=nf(res-1), kernel=1, up=True, resample_kernel=resample_kernel)
-    #             x = (x + t) * (1 / np.sqrt(2))
-    #     return x
-    def block(x, res): # res = 3..resolution_log2
-        with tf.variable_scope('Conv0_up'):
-            x = layer(x, layer_idx=res*2-5, fmaps=nf(res-1), up=True)
-        with tf.variable_scope('Conv1'):
-            x = layer(x, layer_idx=res*2-4, fmaps=nf(res-1))
-        return x
-    def upsample(y):
-        with tf.variable_scope('Upsample'):
-            return upsample_2d(y, k=resample_kernel)
-    def torgb(x, y, res): # res = 2..resolution_log2
-        with tf.variable_scope('ToRGB'):
-            t = apply_bias_act(modulated_conv2d_layer(x, dlatents_in[:, res*2-3], fmaps=num_channels, kernel=1, demodulate=False, fused_modconv=fused_modconv))
-            return t if y is None else y + t
-
-
-    # Early layers.
-    # y = None
-    # with tf.variable_scope('4x4'):
-    #     with tf.variable_scope('Const'):
-    #         x = tf.get_variable('const', shape=[1, nf(1), 4, 4], initializer=tf.initializers.random_normal())
-    #         x = tf.tile(tf.cast(x, dtype), [tf.shape(dlatents_in)[0], 1, 1, 1])
-    #     with tf.variable_scope('Conv'):
-    #         x = layer(x, layer_idx=0, fmaps=nf(1), kernel=3)
-    #     if architecture == 'skip':
-    #         y = torgb(x, y, 2)
-    with tf.variable_scope('4x4'):
-        with tf.variable_scope('Const'):
-            x = tf.get_variable('const', shape=[1, nf(1), 4, 4], initializer=tf.initializers.random_normal())
-            x = tf.tile(tf.cast(x, dtype), [tf.shape(dlatents_in)[0], 1, 1, 1])
-        with tf.variable_scope('Conv'):
-            x = layer(x, layer_idx=0, fmaps=nf(1))
-
-    # Main layers.
-    # for res in range(3, resolution_log2 + 1):
-    #     with tf.variable_scope('%dx%d' % (2**res, 2**res)):
-    #         x = block(x, res)
-    #         if architecture == 'skip':
-    #             y = upsample(y)
-    #         if architecture == 'skip' or res == resolution_log2:
-    #             y = torgb(x, y, res)
-    for res in range(3, resolution_log2 + 1):
-        with tf.variable_scope('%dx%d' % (2**res, 2**res)):
-            x = block(x, res)
-            if res == resolution_log2:
-                y = torgb(x, y, res)
-    images_out = y
-
-    assert images_out.dtype == tf.as_dtype(dtype)
-    return tf.identity(images_out, name='images_out')
 
 #----------------------------------------------------------------------------
 # Original StyleGAN discriminator.
